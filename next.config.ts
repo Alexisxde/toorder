aafd5be12eb31e7485c0e995289735b5ece8b32e
Alexisxde <olivarezalexis749@gmail.com>
2025-03-01 05:59:30 -0300

    chore: Update Next.js configuration and add lazy loading for user avatars

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
 		// Warning: This allows production builds to successfully complete even if
 		// your project has ESLint errors.
 		ignoreDuringBuilds: true
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+				pathname: "/u/**"
+			}
+		]
 	}
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,35 @@
-"use client"
-import { useUser } from "@/context/AuthContext"
-import { useAuthStore } from "@/store/AuthStore"
+import SignOutButton from "@/components/ui/SignOutButton"
+import { createClientForServer } from "@/supabase/server"
 import Image from "next/image"
+import { redirect } from "next/navigation"
 
-export default function Header() {
-	const { signOut } = useAuthStore()
-	const { user } = useUser()
+export default async function Header() {
+	const supabase = createClientForServer()
+	const {
+		data: { user }
+	} = await (await supabase).auth.getUser()
+
+	if (!user) redirect("/sign-in")
+
+	const {
+		user_metadata: { avatar_url, full_name }
+	} = user
 
 	return (
 		<header className="flex items-center justify-between px-10 py-5">
 			<div className="w-24"></div>
 			<div className="flex items-center justify-center gap-4">
-				{user && (
-					<button
-						type="button"
-						className="flex items-center justify-center rounded-lg bg-neutral-800 px-4 py-2 text-center text-xs font-semibold text-white shadow-md transition duration-200 ease-in select-none hover:bg-neutral-700 focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-200 focus:outline-none disabled:cursor-not-allowed disabled:hover:bg-neutral-800"
-						onClick={signOut}>
-						<Image
-							className="mr-2 inline-block size-5 rounded-full object-cover object-center"
-							src={user?.avatar_url}
-							alt={user?.preferred_username}
-						/>
-						Cerrar sesión
-					</button>
-				)}
+				<SignOutButton>
+					<Image
+						className="mr-2 inline-block size-5 rounded-full object-cover object-center"
+						src={avatar_url}
+						width={20}
+						height={20}
+						alt={full_name}
+						loading="lazy"
+					/>
+					Cerrar sesión
+				</SignOutButton>
 			</div>
 		</header>
 	)
