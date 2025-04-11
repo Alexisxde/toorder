dce1f186c37d8a9cb6aafd232ba80731e3bbeb15
Alexisxde <olivarezalexis749@gmail.com>
2025-04-11 16:35:03 -0300

    chore: Update footer component styling and content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="border-b-1 border-neutral-200 bg-neutral-100 p-4 dark:border-neutral-800 dark:bg-neutral-900 text-center text-xs border-t-1 border-neutral-200 dark:border-neutral-800">
+		<footer className="border-t-1 border-neutral-200 bg-neutral-100 p-4 text-center text-xs text-neutral-500 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-400">
 			<p>
 				Construido por {" "}
 				<a
@@ -9,8 +9,8 @@ export default function Footer() {
 					href="https://github.com/Alexisxde">
 					Alexis
 				</a>
-				, el uso de NextJS, Motion, TailwindCSS y Tailwindcss. El el código fuente está
-				disponible en {" "}
+				, el uso de NextJS, Motion, Tailwindcss y Supabase. El el código fuente
+				está disponible en {" "}
 				<a
 					className="underline"
 					target="_blank"
